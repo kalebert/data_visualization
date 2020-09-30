67748f68834b2cd7465e8d772bb2cf1823ae9b60
kalebert <evanschulte9@gmail.com>
2020-09-30 13:23:31 -0600

    Plotting the Random Walk

--- random_walk.py
+++ random_walk.py
@@ -4,12 +4,12 @@ class RandomWalk:
 	"""A class to generate random walks."""
 
 	def __init__(self, num_points=5000):
-	""" Initialize attributes of a walk."""
-	self.num_points = num_points
+		"""Initialize attributes of a walk."""
+		self.num_points = num_points
 
-	# All walks start at (0, 0).
-	self.x_values = [0]
-	self.y_values = [0]
+		# All walks start at (0, 0).
+		self.x_values = [0]
+		self.y_values = [0]
 
 	def fill_walk(self):
 		"""Calculate all points of the walk."""
