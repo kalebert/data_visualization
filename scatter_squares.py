4597c969106b94e8c553a375f41776f37750a072
kalebert <evanschulte9@gmail.com>
2020-09-30 13:09:39 -0600

    Choosing Directions

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,6 +6,8 @@ y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 # ax.scatter(x_values, y_values, c="red", s=10)
+
+# Below code charts graph with 
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
@@ -20,3 +22,6 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 ax.axis([0, 1100, 0, 1100001])
 
 plt.show()
+
+# Use the below code to save graph to a file
+# plt.savefig('squares_plot.png', bbox_inches='tight')
