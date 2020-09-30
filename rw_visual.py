fa5157618441d9b3e30c63c98809830b95e1d0ec
kalebert <evanschulte9@gmail.com>
2020-09-30 16:04:30 -0600

    Cleaning Up the Axes

--- rw_visual.py
+++ rw_visual.py
@@ -14,12 +14,16 @@ while True:
 	fig, ax = plt.subplots()
 	point_numbers = range(rw.num_points)
 	ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, 
-		edgecolors='none' ,s=15)
+		edgecolors='none' ,s=10)
 	ax.scatter(0, 0, c='green', edgecolors='none', s=100)
 	ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
 		s=100)
-	plt.show()
 
+	# Remove the axes
+	ax.get_xaxis().set_visible(False) 
+	ax.get_yaxis().set_visible(False)
+
+	plt.show()
 	keep_running = input("Make another walk? (y/n)")
 	if keep_running == 'n':
 		break
