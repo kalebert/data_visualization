8cc595fb1c8dc009fe6f420d52e422db1d2d3e69
kalebert <evanschulte9@gmail.com>
2020-09-30 18:07:35 -0600

    Making a Histogram

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,7 @@
+from plotly.graph_objs import Bar, Layout 
+from plotly import offline
+
+
 from die import Die
 
 # Create a D6
@@ -5,8 +9,25 @@ die = Die()
 
 # Make some rolls, store results in list.
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 
-print(results)
+# Analyze the results.
+frequencies = []
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+
+# Visualize the results.
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title="Result of rolling one D6 die 1000 times", 
+	xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+
+# print(frequencies)
+# print(sum(frequencies))
