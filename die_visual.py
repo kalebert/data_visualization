ed466985cc4f41473030fddabca5d7ed47acd153
kalebert <evanschulte9@gmail.com>
2020-10-01 07:16:54 -0600

    15-8. Multiplication

--- die_visual.py
+++ die_visual.py
@@ -7,30 +7,30 @@ from die import Die
 # Create two die
 die_1 = Die()
 die_2 = Die()
-die_3 = Die()
 
 # Make some rolls, store results in list.
 results = []
 for roll_num in range(50_000):
-	result = die_1.roll() + die_2.roll() + die_3.roll()
+	result = die_1.roll() * die_2.roll()
 	results.append(result)
 
 # Analyze the results.
 frequencies = []
-max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
-for value in range(3, max_result+1):
+max_result = die_1.num_sides * die_2.num_sides
+for value in range(2, max_result+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
 # Visualize the results.
-x_values = list(range(3, max_result+1))
+x_values = list(range(2, max_result+1))
 data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title="Result of rolling three D6 dice 50,000 times", 
+my_layout = Layout(title="Result of rolling two D6 dice 50,000 times & multipling the result", 
 	xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_x3.html')
+offline.plot({'data': data, 'layout': my_layout}, 
+	filename='d6_Multiplication.html')
 
 # print(frequencies)
 # print(sum(frequencies))
