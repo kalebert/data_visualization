30728cbc84facef1dfcce99d48bda6b0802dabc8
kalebert <evanschulte9@gmail.com>
2020-10-03 17:48:52 -0600

    16_8_recent_quakes

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Explore the structure of the data.
-filename = 'data/eq_data_30_day_m1.json'
+filename = 'data/9_2020_earthquakes.json'
 with open(filename) as f:
 	all_eq_data = json.load(f)
 
