ad41ab40068630114cd7c5f3f367a7894005e62d
kalebert <evanschulte9@gmail.com>
2020-10-03 17:40:43 -0600

    16_6_Refactoring

--- eq_world_map.py
+++ eq_world_map.py
@@ -13,21 +13,13 @@ with open(readable_file, 'w') as f:
 	json.dump(all_eq_data, f, indent=4)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
 
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
-	mag = eq_dict['properties']['mag']
-	lon = eq_dict['geometry']['coordinates'][0]
-	lat = eq_dict['geometry']['coordinates'][1]
-	title = eq_dict['properties']['title']
-	mags.append(mag)
-	lons.append(lon)
-	lats.append(lat)
-	hover_texts.append(title)
-
-# Map all earthquakes 
-# data = [Scattergeo(lon=lons, lat=lats)]
+	mags.append(eq_dict['properties']['mag'])
+	lons.append(eq_dict['geometry']['coordinates'][0])
+	lats.append(eq_dict['geometry']['coordinates'][1])
+	hover_texts.append(eq_dict['properties']['title'])
 
 data = [{
 	'type': 'scattergeo', 
